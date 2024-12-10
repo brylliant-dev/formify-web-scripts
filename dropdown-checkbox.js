292fca15af389f1e17b4f3d2fe8b2bfbf90c0933
Brylliant Dev <153876117+brylliant-dev@users.noreply.github.com>
2024-12-11 04:26:31 +0800

    feat(): create dropdown js

---
+++ dropdown-checkbox.js
@@ -0,0 +1,113 @@
+const runFieldPopulate = ({ target, items, searchFor }) => {
+    const dropdownWrapper = $(`#checkbox-dropdown-wrapper-${target}`);
+    const dropdownList = dropdownWrapper.find(`#checkbox-dropdown-list-${target}`);
+    const checkboxTemplate = dropdownList.find(`#checkbox-field-template`);
+    const multiSelectField = $(`#${target}`);
+    const multiSelectOptions = multiSelectField.find("option");
+    const resultLabel = dropdownWrapper.find(".checkbox-result-label");
+  
+    const getCheckedBoxes = () => {
+      // Use this to retain checked items
+      const checkboxFields = dropdownList.find(".checkbox-field-template");
+      return checkboxFields.filter(function () {
+        return $(this).find(".checkbox-template").is(":checked");
+      });
+    };
+  
+    const renderCollectionItems = (items) => {
+      clearCheckboxes();
+  
+      resultLabel.css("display", items.length === 0 ? "block" : "none");
+      resultLabel.text(
+        `No data found for ${target}! ${
+          items.length === 0 ? `Search for "${searchFor}"` : ""
+        }`
+      );
+  
+      items
+        .filter(
+          (item) =>
+            !getCheckedBoxes()
+              .map((_, c) => $(c).attr("id"))
+              .get()
+              .includes(item.slug)
+        )
+        .forEach((item) => {
+          const cloneField = checkboxTemplate.clone();
+          const checkBox = cloneField.find('input[type="checkbox"]');
+          const label = cloneField.find(".checkbox-template-label");
+          const getOption = multiSelectOptions.filter(function () {
+            return $(this).val() === item.slug;
+          });
+  
+          checkBox.on("change", () => {
+            getOption.prop("selected", !getOption.prop("selected"));
+          });
+  
+          checkBox.prop("checked", getOption.prop("selected"));
+          checkBox.attr("name", item.slug);
+          label.attr("for", item.slug);
+          label.text(item.name);
+          cloneField.css("display", "block");
+          cloneField.attr("id", item.slug);
+  
+          dropdownList.append(cloneField);
+        });
+    };
+  
+    const clearCheckboxes = () => {
+      const remainingBoxes = getCheckedBoxes();
+      dropdownList.empty().append(checkboxTemplate);
+      remainingBoxes.each(function () {
+        dropdownList.append($(this));
+      });
+    };
+  
+    renderCollectionItems(items);
+  };
+  
+  const callFn = async ({ target, searchFor }) => {
+    const xanoApiId = {
+      services: "R9uz4-0k/Get_Services_Items",
+      languages: "R9uz4-0k/Get_Languages_Items",
+    };
+  
+    const response = await fetch(
+      `https://x8ki-letl-twmt.n7.xano.io/api:${xanoApiId[target]}?slug=${
+        searchFor ?? ""
+      }`
+    );
+    const data = await response.json();
+  
+    runFieldPopulate({
+      target,
+      items: data.map((i) => i.fieldData),
+      searchFor,
+    });
+  };
+  
+  callFn({ target: "languages" });
+  callFn({ target: "services" });
+  
+  const searchFieldLanguages = $("#search-field-languages");
+  const searchFieldServices = $("#search-field-services");
+  
+  let debounceTimerLanguages;
+  let debounceTimerServices;
+  
+  // For the "Languages" field
+  searchFieldLanguages.on("keyup", (e) => {
+    clearTimeout(debounceTimerLanguages); // Clear any existing timer
+    debounceTimerLanguages = setTimeout(() => {
+      callFn({ target: "languages", searchFor: e.target.value ?? "" });
+    }, 500); // 500ms debounce
+  });
+  
+  // For the "Services" field
+  searchFieldServices.on("keyup", (e) => {
+    clearTimeout(debounceTimerServices); // Clear any existing timer
+    debounceTimerServices = setTimeout(() => {
+      callFn({ target: "services", searchFor: e.target.value ?? "" });
+    }, 500); // 500ms debounce
+  });
+  
