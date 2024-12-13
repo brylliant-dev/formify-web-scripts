739ebd39b8ba486ebe5d319acccbc68a2855c7c3
Brylliant Dev <153876117+brylliant-dev@users.noreply.github.com>
2024-12-13 08:27:49 +0800

    feat(): add form filler script

---
+++ url-param-form-filler.js
@@ -0,0 +1,43 @@
+function getUrlParameters(paramNames, url = window.location.href) {
+  const params = new URLSearchParams(new URL(url).search);
+  const result = {};
+
+  paramNames.forEach((paramName) => {
+    result[paramName] = params.get(paramName);
+  });
+
+  return result;
+}
+
+// Example usage:
+const paramNames = [
+  'formifyRequestId',
+  'companyName',
+  'country',
+  'city',
+  'firstName',
+  'lastName',
+  'email',
+  'phone',
+];
+
+// Extract parameters from current URL
+const extractedParams = getUrlParameters(paramNames);
+
+const fieldIds = {
+  formifyRequestId: 'formify-request-id',
+  companyName: 'company-name',
+  country: 'country',
+  city: 'city',
+  firstName: 'first-name',
+  lastName: 'last-name',
+  email: 'email',
+  phone: 'phone',
+};
+
+// Use jQuery to populate the fields
+$.each(extractedParams, (key, value) => {
+  if (value) {
+    $(`#${fieldIds[key]}`).val(value);
+  }
+});
