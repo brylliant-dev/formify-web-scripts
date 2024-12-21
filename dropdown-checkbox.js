0e3bccd9ab5bcedec1a068f5924b34b86a93d32f
Brylliant Dev <153876117+brylliant-dev@users.noreply.github.com>
2024-12-21 14:32:57 +0800

    feat(): remove usage of xano and use cms collections instead

--- dropdown-checkbox.js
+++ dropdown-checkbox.js
@@ -1,57 +1,59 @@
 const runFieldPopulate = ({ target, items, searchFor }) => {
-  const dropdownWrapper = $(`#checkbox-dropdown-wrapper-${target}`)
-  const dropdownList = dropdownWrapper.find(`#checkbox-dropdown-list-${target}`)
-  const checkboxTemplate = dropdownList.find(`#checkbox-field-template`)
-  const multiSelectField = $(`#${target}`)
-  const multiSelectOptions = multiSelectField.find("option")
-  const resultLabel = dropdownWrapper.find(".checkbox-result-label")
+  const dropdownWrapper = $(`#checkbox-dropdown-wrapper-${target}`);
+  const dropdownList = dropdownWrapper.find(
+    `#checkbox-dropdown-list-${target}`
+  );
+  const checkboxTemplate = dropdownList.find(`#checkbox-field-template`);
+  const multiSelectField = $(`#${target}`);
+  const multiSelectOptions = multiSelectField.find("option");
+  const resultLabel = dropdownWrapper.find(".checkbox-result-label");
 
   const getCheckedBoxes = () => {
     // Use this to retain checked items
-    const checkboxFields = dropdownList.find(".checkbox-field-template")
+    const checkboxFields = dropdownList.find(".checkbox-field-template");
     return checkboxFields.filter(function () {
-      return $(this).find(".checkbox-template").is(":checked")
-    })
-  }
+      return $(this).find(".checkbox-template").is(":checked");
+    });
+  };
 
   const renderPlaceHolder = () => {
     const capitalizeFirstLetter = (text) => {
-      return text.charAt(0).toUpperCase() + text.slice(1)
-    }
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    };
 
     const truncateText = (text, maxLength = 60) => {
-      return text.length > maxLength ? text.slice(0, maxLength) + "..." : text
-    }
+      return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+    };
 
     const textContents = getCheckedBoxes()
       .find("span")
       .get()
-      .map((c) => c.textContent.trim()) // Capitalize first letter
+      .map((c) => c.textContent.trim()); // Capitalize first letter
 
-    const result = dropdownWrapper.find(".checkbox-dropdown-placeholder")
+    const result = dropdownWrapper.find(".checkbox-dropdown-placeholder");
     if (textContents.length === 0) {
-      result.text(`Pick ${capitalizeFirstLetter(target)}`)
+      result.text(`Pick ${capitalizeFirstLetter(target)}`);
     } else {
-      result.text(truncateText(textContents.join(", ")))
+      result.text(truncateText(textContents.join(", ")));
     }
-  }
+  };
 
   const renderCollectionItems = (items) => {
-    clearCheckboxes()
+    clearCheckboxes();
 
-    resultLabel.css("display", items.length === 0 ? "block" : "none")
+    resultLabel.css("display", items.length === 0 ? "block" : "none");
     resultLabel.text(
       `No data found for ${target}! ${
         items.length === 0 ? `Search for "${searchFor}"` : ""
-      }`,
-    )
+      }`
+    );
 
     const languageFilter = (item) => {
       return !(
         target === "languages" &&
         ["swahili", "chinese", "japanese"].includes(item.slug)
-      )
-    }
+      );
+    };
 
     items
       .filter(
@@ -59,87 +61,90 @@ const runFieldPopulate = ({ target, items, searchFor }) => {
           !getCheckedBoxes()
             .map((_, c) => $(c).attr("id"))
             .get()
-            .includes(item.slug),
+            .includes(item.slug)
       )
       .filter(languageFilter)
       .sort((a, b) => a.slug.localeCompare(b.slug))
       .forEach((item) => {
-        const cloneField = checkboxTemplate.clone()
-        const checkBox = cloneField.find('input[type="checkbox"]')
-        const label = cloneField.find(".checkbox-template-label")
+        const cloneField = checkboxTemplate.clone();
+        const checkBox = cloneField.find('input[type="checkbox"]');
+        const label = cloneField.find(".checkbox-template-label");
         const getOption = multiSelectOptions.filter(function () {
-          return $(this).val() === item.slug
-        })
+          return $(this).val() === item.slug;
+        });
 
         checkBox.on("change", () => {
-          getOption.prop("selected", !getOption.prop("selected"))
+          getOption.prop("selected", !getOption.prop("selected"));
 
-          renderPlaceHolder()
-        })
+          renderPlaceHolder();
+        });
 
-        checkBox.prop("checked", getOption.prop("selected"))
-        checkBox.attr("name", item.slug)
-        label.attr("for", item.slug)
-        label.text(item.name)
-        cloneField.css("display", "block")
-        cloneField.attr("id", item.slug)
+        checkBox.prop("checked", getOption.prop("selected"));
+        checkBox.attr("name", item.slug);
+        label.attr("for", item.slug);
+        label.text(item.name);
+        cloneField.css("display", "block");
+        cloneField.attr("id", item.slug);
 
-        dropdownList.append(cloneField)
-      })
-  }
+        dropdownList.append(cloneField);
+      });
+  };
 
   const clearCheckboxes = () => {
-    const remainingBoxes = getCheckedBoxes()
-    dropdownList.empty().append(checkboxTemplate)
+    const remainingBoxes = getCheckedBoxes();
+    dropdownList.empty().append(checkboxTemplate);
     remainingBoxes.each(function () {
-      dropdownList.append($(this))
-    })
-  }
+      dropdownList.append($(this));
+    });
+  };
 
-  renderCollectionItems(items)
-}
+  renderCollectionItems(items);
+};
 
-const callFn = async ({ target, searchFor }) => {
-  const xanoApiId = {
-    services: "R9uz4-0k/Get_Services_Items",
-    languages: "R9uz4-0k/Get_Languages_Items",
-  }
+const callFn = async ({ target, searchFor = "" }) => {
+  const itemsRaw = document
+    .querySelector(`#code-block-wrapper .${target}-collection-list`)
+    .querySelectorAll(`.${target}-collection-item`);
+  const items = Array.from(itemsRaw)
+    .map((li) => JSON.parse(li.textContent))
+    .filter(({ name, slug }) => {
+      const validator = [name, slug].some((l) =>
+        new RegExp(searchFor.toLowerCase().replace(/\*/g, ".*")).test(
+          l.toLowerCase()
+        )
+      );
 
-  const response = await fetch(
-    `https://x8ki-letl-twmt.n7.xano.io/api:${xanoApiId[target]}?slug=${
-      searchFor ?? ""
-    }`,
-  )
-  const data = await response.json()
+      return searchFor === "" || validator;
+    });
 
   runFieldPopulate({
     target,
-    items: data.map((i) => i.fieldData),
+    items,
     searchFor,
-  })
-}
+  });
+};
 
-callFn({ target: "languages" })
-callFn({ target: "services" })
+callFn({ target: "languages" });
+callFn({ target: "services" });
 
-const searchFieldLanguages = $("#search-field-languages")
-const searchFieldServices = $("#search-field-services")
+const searchFieldLanguages = $("#search-field-languages");
+const searchFieldServices = $("#search-field-services");
 
-let debounceTimerLanguages
-let debounceTimerServices
+let debounceTimerLanguages;
+let debounceTimerServices;
 
 // For the "Languages" field
 searchFieldLanguages.on("keyup", (e) => {
-  clearTimeout(debounceTimerLanguages) // Clear any existing timer
+  clearTimeout(debounceTimerLanguages); // Clear any existing timer
   debounceTimerLanguages = setTimeout(() => {
-    callFn({ target: "languages", searchFor: e.target.value ?? "" })
-  }, 500) // 500ms debounce
-})
+    callFn({ target: "languages", searchFor: e.target.value ?? "" });
+  }, 500); // 500ms debounce
+});
 
 // For the "Services" field
 searchFieldServices.on("keyup", (e) => {
-  clearTimeout(debounceTimerServices) // Clear any existing timer
+  clearTimeout(debounceTimerServices); // Clear any existing timer
   debounceTimerServices = setTimeout(() => {
-    callFn({ target: "services", searchFor: e.target.value ?? "" })
-  }, 500) // 500ms debounce
-})
+    callFn({ target: "services", searchFor: e.target.value ?? "" });
+  }, 500); // 500ms debounce
+});
