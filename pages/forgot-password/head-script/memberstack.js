9a8006e60a75706fe71e0edb4365273dcfeff271
Jemsukie <jemuel.lupo@gmail.com>
2025-02-20 15:18:46 +0800

    feat(): add showing of agency content tag

--- pages/forgot-password/head-script/memberstack.js
+++ pages/forgot-password/head-script/memberstack.js
@@ -1,12 +1,11 @@
-const memberstack = window.$memberstackDom
+const redirectNonLogin = async () => {
+  const memberstack = window.$memberstackDom
 
-const redirectNonLogin = async () =>
   await memberstack.getCurrentMember().then((r) => {
     if (!r.data?.id) {
       window.location.href = "/"
-    } else {
-      $('#Mem-ID').val(r.data.id)
-    }
+    } 
   })
+}
 
 redirectNonLogin()
--- templates/case-study/head-script/memberstack.js
+++ templates/case-study/head-script/memberstack.js
@@ -7,6 +7,9 @@ const checkMember = async () =>
     } else {
       const caseStudyButton = document.getElementById("case-study_update-wrapper")
 
+      const agencyContentTag = document.getElementById("agency_content_tag")
+      const editAgencyButton = document.getElementById("edit-agency-button")
+
       const cmsMemberstackIdElement = document.getElementById("memberstack-id")
       const cmsMemberstackId = cmsMemberstackIdElement
         ? cmsMemberstackIdElement.textContent.trim()
@@ -16,10 +19,14 @@ const checkMember = async () =>
 
       if (cmsMemberstackId === loggedInMemberstackId) {
         caseStudyButton.style.display = "block"
+        agencyContentTag.classList.remove('ml-16')
+        editAgencyButton.style.display = "block"
         //console.log("IDs match. Showing button");
+
       } else {
         //caseStudyButton.style.display = 'none';
         caseStudyButton.remove()
+        editAgencyButton.remove()
         //console.log("IDs do not match...");
       }
       //console.log("CMS Memberstack ID:", cmsMemberstackId);
