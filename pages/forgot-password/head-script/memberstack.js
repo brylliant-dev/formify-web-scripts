40e36af640260a24de433ccbfb8d57a8463285c3
Jemsukie <jemuel.lupo@gmail.com>
2025-02-20 10:22:17 +0800

    feat(): prefill hidden field with memberstack ID

--- pages/forgot-password/head-script/memberstack.js
+++ pages/forgot-password/head-script/memberstack.js
@@ -4,7 +4,9 @@ const redirectNonLogin = async () =>
   await memberstack.getCurrentMember().then((r) => {
     if (!r.data?.id) {
       window.location.href = "/"
-    } 
+    } else {
+      $('#Mem-ID').val(r.data.id)
+    }
   })
 
 redirectNonLogin()
