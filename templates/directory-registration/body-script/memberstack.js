71a88f112b7e6566eee84b25b2a00bbbfacbf3e1
Jemsukie <jemuel.lupo@gmail.com>
2025-02-21 13:40:25 +0800

    feat(): remove directory registration memberstack script

--- templates/directory-registration/body-script/memberstack.js
+++ templates/directory-registration/body-script/memberstack.js
@@ -1,29 +1,27 @@
-const checkMember = async () => {
-  const memberstack = window.$memberstackDom
+// const checkMember = async () => {
+//   const memberstack = window.$memberstackDom
 
-    await memberstack.getCurrentMember().then((r) => {
-    if (r.data?.id) {
-    const caseStudyButton = document.getElementById('case-study_add-wrapper');
+//     await memberstack.getCurrentMember().then((r) => {
+//     if (r.data?.id) {
+//     const caseStudyButton = document.getElementById('case-study_add-wrapper');
 
-    const cmsMemberstackIdElement = document.getElementById('memberstack-id');
-    const cmsMemberstackId = cmsMemberstackIdElement ? cmsMemberstackIdElement.textContent.trim() : null;
+//     const cmsMemberstackIdElement = document.getElementById('memberstack-id');
+//     const cmsMemberstackId = cmsMemberstackIdElement ? cmsMemberstackIdElement.textContent.trim() : null;
         
-    const loggedInMemberstackId = r.data?.id ?? null;
+//     const loggedInMemberstackId = r.data?.id ?? null;
 
-    if (cmsMemberstackId === loggedInMemberstackId) {
-      caseStudyButton.style.display = 'block';
-      //console.log("IDs match. Showing button");
-    } else {
-      //caseStudyButton.style.display = 'none';
-      caseStudyButton.remove();
-      //console.log("IDs do not match...");
-    }
-    //console.log("CMS Memberstack ID:", cmsMemberstackId);
-    //console.log("Logged-in Memberstack ID:", loggedInMemberstackId);
-    }
-  })
-}
+//     if (cmsMemberstackId === loggedInMemberstackId) {
+//       caseStudyButton.style.display = 'block';
+//       //console.log("IDs match. Showing button");
+//     } else {
+//       //caseStudyButton.style.display = 'none';
+//       caseStudyButton.remove();
+//       //console.log("IDs do not match...");
+//     }
+//     //console.log("CMS Memberstack ID:", cmsMemberstackId);
+//     //console.log("Logged-in Memberstack ID:", loggedInMemberstackId);
+//     }
+//   })
+// }
 
-
-
-checkMember()
+// checkMember()
