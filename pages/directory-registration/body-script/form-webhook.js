fa7bffd4d994a5fb424f912897d8f978db6b9f97
Jemsukie <jemuel.lupo@gmail.com>
2025-02-22 11:32:08 +0800

    feat(): revert debugger

--- pages/directory-registration/body-script/form-webhook.js
+++ pages/directory-registration/body-script/form-webhook.js
@@ -82,11 +82,6 @@ Webflow.push(function () {
     let formData = new FormData(event.target);
 
     console.log('Form Data', formData)
-    const debug = true
-    
-    if(debug) {
-      return
-    }
 
     // Validate that the file input has a file
     // `fileUploadSkip` constant is shared from `auto-fill-case-study-form.js`, remember to copy/move this file with it
