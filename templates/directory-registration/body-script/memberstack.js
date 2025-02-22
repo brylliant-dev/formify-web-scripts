c3d269594ab04cee861f9d5247026dd0e5a23a5a
Jemsukie <jemuel.lupo@gmail.com>
2025-02-22 11:40:34 +0800

    feat(): try another script

--- templates/directory-registration/body-script/memberstack.js
+++ templates/directory-registration/body-script/memberstack.js
@@ -1,25 +1,64 @@
+// const checkMember = async () => {
+//   const memberstack = window.$memberstackDom
+
+//     await memberstack.getCurrentMember().then((r) => {
+//     if (r.data?.id) {
+//     const caseStudyButton = document.getElementById('case-study_add-wrapper');
+
+//     const cmsMemberstackIdElement = document.getElementById('memberstack-id');
+//     const cmsMemberstackId = cmsMemberstackIdElement ? cmsMemberstackIdElement.textContent.trim() : null;
+        
+//     const loggedInMemberstackId = r.data?.id ?? null;
+
+//     if (cmsMemberstackId === loggedInMemberstackId) {
+//       caseStudyButton.style.display = 'block';
+//       //console.log("IDs match. Showing button");
+//     } else {
+//       //caseStudyButton.style.display = 'none';
+//       caseStudyButton.remove();
+//       //console.log("IDs do not match...");
+//     }
+//     //console.log("CMS Memberstack ID:", cmsMemberstackId);
+//     //console.log("Logged-in Memberstack ID:", loggedInMemberstackId);
+//     }
+//   })
+// }
+
+// checkMember()
+
 const checkMember = async () => {
   const memberstack = window.$memberstackDom
 
-    await memberstack.getCurrentMember().then((r) => {
-    if (r.data?.id) {
-    const caseStudyButton = document.getElementById('case-study_add-wrapper');
-
-    const cmsMemberstackIdElement = document.getElementById('memberstack-id');
-    const cmsMemberstackId = cmsMemberstackIdElement ? cmsMemberstackIdElement.textContent.trim() : null;
-        
-    const loggedInMemberstackId = r.data?.id ?? null;
-
-    if (cmsMemberstackId === loggedInMemberstackId) {
-      caseStudyButton.style.display = 'block';
-      //console.log("IDs match. Showing button");
+  await memberstack.getCurrentMember().then((r) => {
+    if (!r.data?.id) {
+      window.location.href = "/"
     } else {
-      //caseStudyButton.style.display = 'none';
-      caseStudyButton.remove();
-      //console.log("IDs do not match...");
-    }
-    //console.log("CMS Memberstack ID:", cmsMemberstackId);
-    //console.log("Logged-in Memberstack ID:", loggedInMemberstackId);
+      const caseStudyButton = document.getElementById("case-study_update-wrapper")
+
+      const agencyContentTag = document.getElementById("agency_content_tag")
+      const editAgencyButton = document.getElementById("edit-agency-button")
+
+      const cmsMemberstackIdElement = document.getElementById("memberstack-id")
+      const cmsMemberstackId = cmsMemberstackIdElement
+        ? cmsMemberstackIdElement.textContent.trim()
+        : null
+
+      const loggedInMemberstackId = r.data?.id ?? null
+
+      if (cmsMemberstackId === loggedInMemberstackId) {
+        caseStudyButton.style.display = "block"
+        editAgencyButton.style.display = "block"
+        agencyContentTag.classList.add('ml-16')
+        //console.log("IDs match. Showing button");
+
+      } else {
+        //caseStudyButton.style.display = 'none';
+        caseStudyButton.remove()
+        editAgencyButton.remove()
+        //console.log("IDs do not match...");
+      }
+      //console.log("CMS Memberstack ID:", cmsMemberstackId);
+      //console.log("Logged-in Memberstack ID:", loggedInMemberstackId);
     }
   })
 }
